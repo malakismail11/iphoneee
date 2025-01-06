e1b71553f7e5c68346f1ee33f9d9bc484497b9fd
malakismail11 <malamismael757@gmail.com>
2025-01-06 16:04:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let img=document.getElementById('img')
+let container=document.querySelector('.container')
+function phones(phone){
+ img.src=phone;
+ console.log(img)
+
+}
+function colors(color){
+    container.style.background=color;
+    console.log(container)
+}
+
